7e567095a836568e2ad0c8a37399296954b41c65
jasvant <jasvantyadav7081@gmail.com>
2026-03-18 14:47:58 +0530

    [jasvant] new changes

--- opencart/src/test/java/pageObjects/HomePage.java
+++ opencart/src/test/java/pageObjects/HomePage.java
@@ -1,6 +1,41 @@
 package pageObjects;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+	
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(xpath ="//span[normalize-space()='My Account']" )
+	WebElement btnMyAccount;
+	
+	@FindBy(xpath = "//a[normalize-space()='Register']")
+	WebElement btnRegister;
+	
+	@FindBy(xpath = "//a[normalize-space()='Login']")
+	WebElement btnLogin;
+	
+	public void clickMyAccount() {
+		btnMyAccount.click();
+	}
+	
+	public void clickRegister() {
+		btnRegister.click();
+	}
+	
+	public void clickLogin() {
+		btnLogin.click();
+	}
+	
+	public boolean verifyHomePage() {
+		return  driver.getCurrentUrl().contains("home");
+	}
+	
+	
 	
 	
 
--- opencart/src/test/java/testBase/BaseClass.java
+++ opencart/src/test/java/testBase/BaseClass.java
@@ -1,5 +1,37 @@
 package testBase;
 
-public class BaseClass  {
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass  {
+	
+	public Properties properties;
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setup()throws IOException {
+		
+		 properties=new Properties();
+		 FileReader fileReader=new FileReader("src\\test\\resources\\config.properties");
+		 properties.load(fileReader);
+		 
+		 driver=new ChromeDriver();
+		 driver.get(properties.getProperty("url"));
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 driver.manage().deleteAllCookies();
+		 	
+	}
+	
+	@AfterClass
+	public void teardown() {
+		driver.quit();
+	}
 }
